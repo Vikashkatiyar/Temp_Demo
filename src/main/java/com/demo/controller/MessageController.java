7b0439bab45113559eecf74acef48dfa64cecfd8
Vikash Katiyar <rishabktr018@gmail.com>
2025-03-02 00:36:25 +0530

    getMessage first commit

---
+++ src/main/java/com/demo/controller/MessageController.java
@@ -0,0 +1,16 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/{name}")
+    public String getMessage(@PathVariable String name){
+        return "Hello " + name + ", How are you ?";
+    }
+
+}
