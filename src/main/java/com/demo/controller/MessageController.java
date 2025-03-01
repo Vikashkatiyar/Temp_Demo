a64d50a80a4f48bed04a6a78b913ddcdc89213dc
Vikash Katiyar <rishabktr018@gmail.com>
2025-03-02 00:49:47 +0530

    getMessage second Commit

--- src/main/java/com/demo/controller/MessageController.java
+++ src/main/java/com/demo/controller/MessageController.java
@@ -3,9 +3,11 @@ package com.demo.controller;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/getMessage")
 public class MessageController {
 
     @GetMapping("/{name}")
